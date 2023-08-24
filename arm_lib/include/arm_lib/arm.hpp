f0593d3367f71d98fe413d63bcf70a35f510dc66
Oyaki122 <oc.yaki122@gmail.com>
2023-08-24 20:09:43 +0900

    arm_libを大体作成

--- arm_lib/include/arm_lib/arm.hpp
+++ arm_lib/include/arm_lib/arm.hpp
@@ -1,13 +1,51 @@
 #pragma once
 
 #include "rclcpp/rclcpp.hpp"
+#include "rogilink2_interfaces/msg/frame.hpp"
+#include <rclcpp/publisher.hpp>
+#include <rclcpp/subscription.hpp>
+#include <rclcpp/timer.hpp>
+#include <rogilink2_interfaces/msg/detail/frame__struct.hpp>
+#include <string>
+
+using namespace std::placeholders;
+using namespace std::chrono_literals;
 
 class Arm {
 public:
-  Arm(rclcpp::Node *node, uint8_t hard_id);
+  Arm(rclcpp::Node *node, std::string deviceName);
+
+  enum ZMode { vel = 0, pos = 1 };
+  enum ArmAngle { ownArea, commonArea };
+
+  void setZMode(ZMode mode);
+  void setZPos(float pos);
+  void setZVel(float vel);
+
+  void setArmAngle(ArmAngle angle);
+
+  void setHand(bool hand0, bool hand1, bool hand2);
+
+  float getZPos();
 
 private:
   const rclcpp::Node *node;
-  const uint8_t hard_id;
+  const std::string deviceName;
+  rclcpp::Publisher<rogilink2_interfaces::msg::Frame>::SharedPtr pub;
+  rclcpp::Subscription<rogilink2_interfaces::msg::Frame>::SharedPtr sub;
+  rclcpp::TimerBase::SharedPtr timer;
+
+  void rogilinkCallback(const rogilink2_interfaces::msg::Frame::SharedPtr msg);
+  void timerCallback();
+
+  ZMode zMode = pos;
+  ArmAngle armAngle = ownArea;
+  bool handState[3] = {0};
+
+  float zPos = 0;
+  float zVel = 0;
+
+  float currentZPos = 0;
+
 };
-
+
