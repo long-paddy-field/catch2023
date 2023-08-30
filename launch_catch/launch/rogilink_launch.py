03d9cf69b2741cce0902a3136d612afe3d6d1586
Oyaki122 <oc.yaki122@gmail.com>
2023-08-30 17:38:36 +0900

    name修正

--- launch_catch/launch/rogilink_launch.py
+++ launch_catch/launch/rogilink_launch.py
@@ -6,23 +6,20 @@ from ament_index_python.packages import get_package_share_directory
 
 
 def generate_launch_description():
-    return LaunchDescription(
-        [
-            Node(
-                package="rogilink2",
-                executable="rogilink2",
-                name="rogilink",
-                output="screen",
-                emulate_tty=True,
-                parameters=[
-                    {
-                        "config_path": os.path.join(
-                            get_package_share_directory("launch_catch"),
-                            "config",
-                            "rogilink.yaml",
-                        )
-                    }
-                ],
-            )
-        ]
-    )
+    return LaunchDescription([
+        Node(
+            package="rogilink2",
+            executable="rogilink2",
+            name="rogilink2",
+            output="screen",
+            emulate_tty=True,
+            parameters=[{
+                "config_path":
+                os.path.join(
+                    get_package_share_directory("launch_catch"),
+                    "config",
+                    "rogilink.yaml",
+                )
+            }],
+        )
+    ])
